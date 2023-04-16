6acc917c62dc79913b8f50bc6b07b55ab753e3b0
minetake01 <minetake01.m@gmail.com>
2023-04-17 00:25:48 +0900

    [add] コマンドの外枠を作成

--- src/commands/translate.rs
+++ src/commands/translate.rs
@@ -1,13 +1,22 @@
-use poise::serenity_prelude::{Error, Role};
+use poise::{serenity_prelude::{Channel, futures}, futures_util::{Stream, StreamExt}};
 
 use crate::{Data, AppError};
 
 type Context<'a> = poise::Context<'a, Data, AppError>;
 
+async fn autocomplete_lang<'a>(
+    _ctx: Context<'_>,
+    partial: &'a str,
+) -> impl Stream<Item = String> + 'a {
+    futures::stream::iter(&["BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"])
+        .filter(move |name| futures::future::ready(name.starts_with(partial)))
+        .map(|name| name.to_string())
+}
+
 #[poise::command(
     slash_command,
     hide_in_help,
-    subcommands("set", "info")
+    subcommands("new", "info")
 )]
 pub async fn translate(_: Context<'_>) -> Result<(), AppError> { Ok(()) }
 
@@ -16,14 +25,27 @@ pub async fn translate(_: Context<'_>) -> Result<(), AppError> { Ok(()) }
     slash_command,
     description_localized("ja", "このチャンネルに自動翻訳を設定します。"),
 )]
-pub async fn set(
+pub async fn new(
     ctx: Context<'_>,
-    #[description = "Name of the role group to create"]
-    #[description_localized("ja", "作成するロールグループの名前")]
-    name: String,
-    #[description = "Whether to create in flexible mode"]
-    #[description_localized("ja", "フレキシブルモードで作成するかどうか")]
-    flexible: Option<bool>,
+    #[description = "Name of the automatic translation group"]
+    #[description_localized("ja", "自動翻訳グループの名前")]
+    group: String,
+    #[description = "Channel language"]
+    #[description_localized("ja", "チャンネルの言語")]
+    #[autocomplete = "autocomplete_lang"]
+    lang: String,
+    #[description = "Channel to create an automatic translation group"]
+    #[description_localized("ja", "自動翻訳グループを作成するチャンネル")]
+    channel: Option<Channel>,
+    #[description = "Whether to automatically create a thread for each language when a thread is created"]
+    #[description_localized("ja", "スレッドが作成された時に、自動で各言語用スレッドを作成するか")]
+    auto_threading: Option<bool>,
+    #[description = "Whether to translate thread titles when automatically creating threads"]
+    #[description_localized("ja", "自動でスレッドを作成する際、スレッドのタイトルを翻訳するか")]
+    translate_title: Option<bool>,
+    #[description = "Whether the BOT reacts on behalf of the target message when a reaction is attached remains uncertain"]
+    #[description_localized("ja", "メッセージにリアクションが付いた時に、翻訳先のメッセージにBOTが代理でリアクションを付けるか")]
+    reaction_proxy: Option<bool>,
 ) -> Result<(), AppError> {
     Ok(())
 }
--- src/handler.rs
+++ src/handler.rs
@@ -1,10 +1,20 @@
-use poise::{serenity_prelude::{Context, ChannelId, WebhookId, Webhook}, FrameworkContext, Event};
+use poise::{serenity_prelude::{Context, ChannelId, Webhook}, FrameworkContext, Event};
+use sea_orm::EntityTrait;
 use regex::Regex;
-use deepl::TagHandling::Xml;
 
-use crate::{data::{Data, ChannelAttrs}, AppError};
+use crate::entities::guild::{Model as GuildModel, Entity as GuildEntity};
+use crate::entities::channel::{Model as ChannelModel, Entity as ChannelEntity};
+use crate::entities::translation_group::{Model as TranslationGroupModel, Entity as TranslationGroupEntity};
+use crate::entities::message::{Model as MessageModel, Entity as MessageEntity};
 
-pub async fn event_handler(ctx: &Context, event: &Event<'_>, _framework: FrameworkContext<'_, Data, AppError>, data: &Data) -> Result<(), AppError> {
+use crate::{Data, AppError};
+
+pub async fn event_handler(
+    ctx: &Context,
+    event: &Event<'_>,
+    _framework: FrameworkContext<'_, Data, AppError>,
+    data: &Data
+) -> Result<(), AppError> {
     match event {
         Event::Ready { data_about_bot } => {
             println!("{} is connected! Shard: {}", data_about_bot.user.name, ctx.shard_id);
@@ -14,160 +24,83 @@ pub async fn event_handler(ctx: &Context, event: &Event<'_>, _framework: Framewo
             println!("Cache Ready. {} Guilds Loaded.", guilds.len());
             Ok(())
         },
+        Event::GuildDelete { incomplete, full } => {
+            // unavailableがtrueの場合は、障害などでサーバーから切断されたとき
+            if incomplete.unavailable { return Ok(()) }
+            let Some(guild) = full else { return Ok(()) };
+
+            let delete_guild: Option<GuildModel> = GuildEntity::find_by_id(guild.id.0).one(&data.db).await?;
+            Ok(())
+        },
         Event::ChannelDelete { channel } => {
-            let mut guild_map = data.guild_map.lock().await;
-            guild_map.remove_channel(&channel.guild_id, &channel.id);
             Ok(())
         },
         Event::ThreadCreate { thread } => {
-            let mut guild_map = data.guild_map.lock().await;
-            let mut created_channels: Vec<(ChannelId, ChannelAttrs)> = vec![];
-            {
-                let Some(guild_settings) = guild_map.guild(&thread.guild_id) else { return Ok(()) };
-    
-                // ループ防止
-                if guild_settings.channel_map.channel(&thread.id).is_some() { return Ok(()) }
-    
-                let Some(parent_attrs) = guild_settings.channel_map.channel(&thread.parent_id.unwrap()) else { return Ok(()) };
-                let Some(group_data) = guild_settings.group_map.name(&parent_attrs.group_name) else { return Ok(()) };
-                if parent_attrs.auto_threading {
-                    for to_channel in &group_data.channels {
-                        let Some(to_channel_attrs) = guild_settings.channel_map.channel(&to_channel) else { return Ok(()) };
-    
-                        let mut thread_name = thread.name.clone();
-                        //スレッド名翻訳
-                        if parent_attrs.thread_title {
-                            thread_name = data.deepl
-                                .translate_text(thread_name, to_channel_attrs.lang.clone())
-                                .source_lang(parent_attrs.lang.clone())
-                                .await?
-                                .translations[0]
-                                .text
-                                .clone();
-                        }
-        
-                        // スレッドのIDは元メッセージのIDと同じらしい。
-                        let created_thread = to_channel.create_public_thread(&ctx, thread.id.0, |t| {
-                            t
-                                .name(thread_name)
-                                .kind(thread.kind)
-                        }).await?;
-
-                        created_channels.push((created_thread.id, to_channel_attrs.clone()));
-                    }
-                }
-            }
-            for (channel_id, channel_attrs) in created_channels {
-                guild_map.add_channel(thread.guild_id, channel_id, channel_attrs);
-            }
             Ok(())
         },
         Event::ThreadDelete { thread } => {
-            let mut guild_map = data.guild_map.lock().await;
-            guild_map.remove_channel(&thread.guild_id, &thread.id);
             Ok(())
         },
         Event::ThreadUpdate { thread } => {
-            let guild_map = data.guild_map.lock().await;
-            let Some(guild_settings) = guild_map.guild(&thread.guild_id) else { return Ok(()) };
-            let Some(channel_attrs) = guild_settings.channel_map.channel(&thread.id) else { return Ok(()) };
-            let Some(group_data) = guild_settings.group_map.name(&channel_attrs.group_name) else { return Ok(()) };
-            if channel_attrs.thread_title {
-                for to_channel_id in &group_data.channels {
-                    let Some(to_channel_attrs) = guild_settings.channel_map.channel(&to_channel_id) else { return Ok(()) };
-                    //スレッド名翻訳
-                    let thread_name = data.deepl
-                        .translate_text(thread.name(), to_channel_attrs.lang.clone())
-                        .source_lang(channel_attrs.lang.clone())
-                        .await?
-                        .translations[0]
-                        .text
-                        .clone();
-
-                    to_channel_id.edit_thread(&ctx, |t| {
-                        t.name(thread_name)
-                    }).await?;
-                }
-            }
             Ok(())
         },
         Event::Message { new_message } => {
-            // BOT、Webhook、DMを除外
-            if new_message.webhook_id.is_some() || new_message.author.bot || new_message.is_private() { return Ok(()) };
+            // // BOT、Webhook、DMを除外
+            // if new_message.webhook_id.is_some() || new_message.author.bot || new_message.is_private() { return Ok(()) };
 
-            let mut msg_content = new_message.content.clone();
-            // URLをタグに置換
-            let elements = convert_placeholder(&mut msg_content);
+            // let mut msg_content = new_message.content.clone();
+            // // URLをタグに置換
+            // let elements = convert_placeholder(&mut msg_content);
             
-            let mut guild_map = data.guild_map.lock().await;
-            let guild_id = new_message.guild_id.unwrap();
-            let Some(guild_settings) = guild_map.guild_mut(&guild_id) else { return Ok(()) };
+            // let mut guild_map = data.guild_map.lock().await;
+            // let guild_id = new_message.guild_id.unwrap();
+            // let Some(guild_settings) = guild_map.guild_mut(&guild_id) else { return Ok(()) };
 
-            let Some(channel_attrs) = guild_settings.channel_map.channel(&new_message.channel_id) else { return Ok(()) };
-            let Some(group_data) = guild_settings.group_map.name(&channel_attrs.group_name) else { return Ok(()) };
+            // let Some(channel_attrs) = guild_settings.channel_map.channel(&new_message.channel_id) else { return Ok(()) };
+            // let Some(group_data) = guild_settings.group_map.name(&channel_attrs.group_name) else { return Ok(()) };
             
-            let mut message_ids = vec![];
-            for to_channel_id in group_data.channels.iter() {
-                let Some(to_channel_attrs) = guild_settings.channel_map.channel(to_channel_id) else { return Ok(()) };
-                // メッセージ翻訳
-                let translated_msg = &mut data.deepl
-                    .translate_text(&msg_content, to_channel_attrs.lang.clone())
-                    .source_lang(channel_attrs.lang.clone())
-                    .tag_handling(Xml)
-                    .await?
-                    .translations[0]
-                    .text;
+            // let mut message_ids = vec![];
+            // for to_channel_id in group_data.channels.iter() {
+            //     let Some(to_channel_attrs) = guild_settings.channel_map.channel(to_channel_id) else { return Ok(()) };
+            //     // メッセージ翻訳
+            //     let translated_msg = &mut data.deepl
+            //         .translate_text(&msg_content, to_channel_attrs.lang.clone())
+            //         .source_lang(channel_attrs.lang.clone())
+            //         .tag_handling(Xml)
+            //         .await?
+            //         .translations[0]
+            //         .text;
 
-                // タグをURLに置換
-                revert_placeholder(translated_msg, &elements);
+            //     // タグをURLに置換
+            //     revert_placeholder(translated_msg, &elements);
 
-                let Ok(webhook) = get_webhook(&ctx, to_channel_id).await else { continue };
-                let Some(message) = webhook
-                    .execute(&ctx, false, |w| {
-                        w
-                            .content(&translated_msg)
-                            .add_files(new_message.attachments.iter().map(|attachment| attachment.url.as_str()))
-                            .username(format!("{} (Auto translated)", new_message.author.name))
-                            .avatar_url(new_message.author.avatar_url().unwrap_or(new_message.author.default_avatar_url()))
-                    }).await? else { continue };
+            //     let Ok(webhook) = get_webhook(&ctx, to_channel_id).await else { continue };
+            //     let Some(message) = webhook
+            //         .execute(&ctx, false, |w| {
+            //             w
+            //                 .content(&translated_msg)
+            //                 .add_files(new_message.attachments.iter().map(|attachment| attachment.url.as_str()))
+            //                 .username(format!("{} (Auto translated)", new_message.author.name))
+            //                 .avatar_url(new_message.author.avatar_url().unwrap_or(new_message.author.default_avatar_url()))
+            //         }).await? else { continue };
                 
-                message_ids.push(message.id);
-            }
-            guild_map.add_or_update_message(&guild_id, &new_message.channel_id, new_message.id, message_ids);
+            //     message_ids.push(message.id);
+            // }
+            // guild_map.add_or_update_message(&guild_id, &new_message.channel_id, new_message.id, message_ids);
             Ok(())
         },
         Event::MessageDelete { channel_id, deleted_message_id, guild_id } => {
-            let Some(guild_id) = guild_id else { return Ok(()) };
-            let mut guild_map = data.guild_map.lock().await;
-            let Some(message_ids) = guild_map.remove_message(guild_id, channel_id, deleted_message_id) else { return Ok(()) };
-            for message_id in message_ids {
-                let Ok(webhook) = get_webhook(&ctx, channel_id).await else { continue };
-                webhook.delete_message(&ctx, message_id).await?;
-            }
             Ok(())
         },
         Event::MessageDeleteBulk { channel_id, multiple_deleted_messages_ids, guild_id } => {
-            let Some(guild_id) = guild_id else { return Ok(()) };
-            let mut guild_map = data.guild_map.lock().await;
-            for message_id in multiple_deleted_messages_ids.iter() {
-                let Some(message_ids) = guild_map.remove_message(guild_id, channel_id, message_id) else { continue };
-                for message_id in message_ids {
-                    let Ok(webhook) = get_webhook(&ctx, channel_id).await else { continue };
-                    webhook.delete_message(&ctx, message_id).await?;
-                }
-            }
             Ok(())
         },
         Event::MessageUpdate { old_if_available: _, new, event } => {
-            let Some(message) = new else { return Ok(()) };
-            // BOT、Webhook、DMを除外
-            if message.webhook_id.is_some() || message.author.bot || message.is_private() { return Ok(()) };
             Ok(())
         },
         Event::ReactionAdd { add_reaction } => todo!(),
         Event::ReactionRemove { removed_reaction } => todo!(),
         Event::ReactionRemoveAll { channel_id, removed_from_message_id } => todo!(),
-        Event::GuildDelete { incomplete, full } => todo!(),
         event => {
             #[cfg(debug_assertions)]
             println!("{}", event.name());
