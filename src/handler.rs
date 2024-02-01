890e901e706c211b16d1208e503d3ddaad10d0f4
minetake01 <minetake01.m@gmail.com>
2024-02-01 10:13:47 +0900

    実装予定のイベントにtodoマクロを配置

--- src/handler.rs
+++ src/handler.rs
@@ -30,11 +30,17 @@ pub async fn event_handler<'a>(
         FullEvent::ShardsReady { total_shards } => shard_ready(total_shards),
         FullEvent::GuildDelete { incomplete, full } => guild_delete(ctx, data, incomplete, full).await,
         FullEvent::ChannelDelete { channel, messages } => channel_delete(ctx, data, channel, messages).await,
+        FullEvent::ChannelPinsUpdate { pin } => todo!("ChannelPinsUpdate"),
         FullEvent::ThreadCreate { thread } => thread_create(ctx, data, thread).await,
-        FullEvent::Message { new_message } => {
-            println!("Message: {}", new_message.content);
-            Ok(())
-        },
+        FullEvent::ThreadUpdate { old, new } => todo!("ThreadUpdate"),
+        FullEvent::ThreadDelete { thread, full_thread_data } => todo!("ThreadDelete"),
+        FullEvent::Message { new_message } => todo!("Message"),
+        FullEvent::MessageUpdate { old_if_available, new, event } => todo!("MessageUpdate"),
+        FullEvent::MessageDelete { channel_id, deleted_message_id, guild_id } => todo!("MessageDelete"),
+        FullEvent::MessageDeleteBulk { channel_id, multiple_deleted_messages_ids, guild_id } => todo!("MessageDeleteBulk"),
+        FullEvent::ReactionAdd { add_reaction } => todo!("ReactionAdd"),
+        FullEvent::ReactionRemove { removed_reaction } => todo!("ReactionRemove"),
+        FullEvent::ReactionRemoveAll { channel_id, removed_from_message_id } => todo!("ReactionRemoveAll"),
         event => {
             #[cfg(debug_assertions)]
             println!("Unhandled Event: {}", event.snake_case_name());
