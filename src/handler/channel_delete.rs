95930a56626001dc7cc7eb5d096c6db86d8996b4
minetake01 <minetake01.m@gmail.com>
2024-02-01 10:59:43 +0900

    不要な引数を除去

--- src/handler/channel_delete.rs
+++ src/handler/channel_delete.rs
@@ -1,9 +1,9 @@
-use poise::serenity_prelude::{Context, GuildChannel, Message};
+use poise::serenity_prelude::GuildChannel;
 use sea_orm::EntityTrait;
 
 use crate::{AppError, Data, entities::channel::Entity as ChannelEntity};
 
-pub async fn channel_delete(_ctx: &Context, data: &Data, channel: &GuildChannel, _messages: &Option<Vec<Message>>) -> Result<(), AppError> {
+pub async fn channel_delete(data: &Data, channel: &GuildChannel) -> Result<(), AppError> {
     ChannelEntity::delete_by_id(channel.id)
         .exec(&data.db)
         .await?;
--- src/handler/guild_delete.rs
+++ src/handler/guild_delete.rs
@@ -1,9 +1,9 @@
-use poise::serenity_prelude::{Context, UnavailableGuild, Guild};
+use poise::serenity_prelude::{UnavailableGuild, Guild};
 use sea_orm::EntityTrait;
 
 use crate::{AppError, Data, entities::guild::Entity as GuildEntity};
 
-pub async fn guild_delete(_ctx: &Context, data: &Data, incomplete: &UnavailableGuild, full: &Option<Guild>) -> Result<(), AppError> {
+pub async fn guild_delete(data: &Data, incomplete: &UnavailableGuild, full: &Option<Guild>) -> Result<(), AppError> {
     // unavailableがtrueの場合は、障害などでサーバーから切断されたとき
     if incomplete.unavailable { return Ok(()) }
     let Some(guild) = full else { return Ok(()) };
